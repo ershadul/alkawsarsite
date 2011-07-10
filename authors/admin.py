4f6adac756ac3cc9661ae3f99c8545482d414893
unknown <ershadul@.(none)>
2011-07-10 07:53:44 +0600

    Fixed authors

--- authors/admin.py
+++ authors/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from alkawsarsite.authors.models import Author
 
 class AuthorAdmin(admin.ModelAdmin):
-        search_fields = ['name', 'email', 'guid']
-	list_filter = ('language',)
-	list_display = ('id', 'language', 'name', 'email', 'guid',)
+    search_fields = ['name', 'email']
+    list_filter = ('language',)
+    list_display = ('id', 'language', 'name', 'email', 'order',)
+    list_editable = ('order',)
 
-        class Media:
-                js = ('/static/js/jquery-1.4.2.min.js',
-                      '/static/js/tiny_mce/tiny_mce.js',
-                      '/static/js/textareas.js',)
+    class Media:
+        js = ('/static/js/jquery-1.4.2.min.js',
+              '/static/js/tiny_mce/tiny_mce.js',
+              '/static/js/textareas.js',)
 
 admin.site.register(Author, AuthorAdmin)
--- settings.py
+++ settings.py
@@ -71,7 +71,7 @@ TEMPLATE_DIRS = (
 
 TEMPLATE_CONTEXT_PROCESSORS  = (
     #'django.core.context_processors.csrf',
-    #"django.core.context_processors.auth",
+    "django.contrib.auth.context_processors.auth",
 )
 
 INSTALLED_APPS = (
